7980aec7377203f0efb7ce8d1e2ae4be90af3f0c
karifol <karigolhorinouchi@gmail.com>
2024-06-16 09:38:14 +0900

    Initial Commit

---
+++ Okashi/OkashiApp.swift
@@ -0,0 +1,17 @@
+//
+//  OkashiApp.swift
+//  Okashi
+//
+//  Created by 堀ノ内海斗 on 2024/06/16.
+//
+
+import SwiftUI
+
+@main
+struct OkashiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
